a50bc5d11c7af8b3f44e49479435715f5c67c59f
Noel1409 <70179169+Noel1409@users.noreply.github.com>
2020-08-24 17:34:48 -0600

    Add files via upload
    Archivos de Práctica Primer Parcial

---
+++ AlgoritmoProcesos/src/algoritmoprocesos/AlgoritmoProcesos.java
@@ -0,0 +1,18 @@
+package algoritmoprocesos;
+
+/**
+ *
+ * @author Jaime Noel López Daniel 0901-18-735
+ */
+public class AlgoritmoProcesos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Apertura de la ventana gráfica
+        Algoritmos Ventana = new Algoritmos();
+        Ventana.setVisible(true);
+    }
+    
+}
